8a3ccd8419286d9438fb7622fbd1e83c4490aa4f
fbr1 <fbr1@users.noreply.github.com>
2015-04-23 20:39:56 -0300

    fixed mutation

--- Ejercicios/Ejercicio1/Program.cs
+++ Ejercicios/Ejercicio1/Program.cs
@@ -12,11 +12,12 @@ namespace Ejercicio1
         const int POBLACION_SIZE = 10;
         const int CICLOS = 20;
         const double PROB_CROSSOVER = 0.75;
-        const double PROB_MUTACION = 0.05;
+        const double PROB_MUTACION = 1;
 
         // Metodos
         static void Main(string[] args)
         {
+            double maximo=0;
             Individuo[] individuos = new Individuo[POBLACION_SIZE];
 
 
@@ -34,20 +35,20 @@ namespace Ejercicio1
             foreach(Individuo ind in individuos)
             {
                 suma = suma + ind.FuncionObjetiva;
+                if(ind.FuncionObjetiva>maximo){
+                    maximo = ind.FuncionObjetiva;
+                }
             }           
             foreach (Individuo ind in individuos)
             {
                 ind.Fitness = ind.FuncionObjetiva / suma;                
             }
-			
-            Console.WriteLine();
-            Console.WriteLine("Suma= " + suma);
-            Console.WriteLine();     
 
-            foreach (Individuo ind in individuos)
-            {
-                Console.WriteLine(ind.Fitness);
-            }
+            Console.WriteLine();
+            Console.WriteLine("Suma= " + suma);            
+            Console.WriteLine("Promedio= " + suma/POBLACION_SIZE);            
+            Console.WriteLine("Maximo= " + maximo);
+            Console.WriteLine();
 
             // Seleccion
             
@@ -59,8 +60,7 @@ namespace Ejercicio1
             Console.WriteLine();
 
             for(int i = 1; i<POBLACION_SIZE; i++){
-                fitnessAcumulado[i] = fitnessAcumulado[i - 1] + individuos[i].Fitness;
-                Console.WriteLine(fitnessAcumulado[i]);
+                fitnessAcumulado[i] = fitnessAcumulado[i - 1] + individuos[i].Fitness;                
             }
 
             Console.WriteLine();
@@ -73,8 +73,7 @@ namespace Ejercicio1
                 if(indice < 0){
                     indice = Math.Abs(indice + 1);
                 }
-                seleccionados[i] = individuos[indice];
-                Console.WriteLine("Random Number: "+ random_number + " Fitness: " + seleccionados[i].Fitness + " Indice: " + indice); 
+                seleccionados[i] = individuos[indice];                
             }
 
             Individuo[] nuevaGeneracion = new Individuo[POBLACION_SIZE];
@@ -98,54 +97,57 @@ namespace Ejercicio1
                             segundoPadre = temp;
                         }
 
-                        nuevaGeneracion[i].Cromosoma[j] = individuos[primerPadre].Cromosoma[j];
-                        nuevaGeneracion[i + 1].Cromosoma[j] = individuos[segundoPadre].Cromosoma[j];                        
+                        nuevaGeneracion[i].Cromosoma[j] = seleccionados[primerPadre].Cromosoma[j];
+                        nuevaGeneracion[i + 1].Cromosoma[j] = seleccionados[segundoPadre].Cromosoma[j];                        
                     }
-                    // Mutacion
+                    // Mutation
                     for (int k = 0; k < 2; k++)
                     {
-                        if (rnd.NextDouble() < 1)
+                        if (rnd.NextDouble() < PROB_MUTACION)
                         {
-                            int comienzo = 0;
-                            int fin = 0;
-                            do
-                            {
-                                comienzo = rnd.Next(0, Individuo.CROMOSOMA_SIZE);
-                                fin = rnd.Next(0, Individuo.CROMOSOMA_SIZE);
-                                if (comienzo > fin)
-                                {
-                                    int temp = comienzo;
-                                    comienzo = fin;
-                                    fin = temp;
-                                }
-                            } while (comienzo == fin);
-                            int diferencia = fin - comienzo;
-                            Console.WriteLine(comienzo + "," + fin + "," + diferencia);
-                            int[] temp2 = nuevaGeneracion[i + k].Cromosoma;
-                            for (int j = 0; j <= diferencia; j++)
-                            {
-                                
-                                nuevaGeneracion[i + k].Cromosoma[comienzo + j] = temp2[fin - j];
-                                Console.WriteLine((comienzo + j) + " = " + nuevaGeneracion[i + k].Cromosoma[comienzo + j] + "," + (fin - j) + " = " + temp2[fin - j]);
-                            }
-                            Console.WriteLine(string.Join("", temp2));                            
-                            Console.WriteLine(string.Join("", nuevaGeneracion[i+k].Cromosoma));
-                            Console.WriteLine("-------------------");
+                            Mutar(nuevaGeneracion[i + k].Cromosoma, rnd);
                         }
                     }
-
-
                 }
                 else
                 {
-                    nuevaGeneracion[i] = individuos[i];
-                    nuevaGeneracion[i+1] = individuos[i+1];
+                    nuevaGeneracion[i] = seleccionados[i];
+                    nuevaGeneracion[i + 1] = seleccionados[i + 1];
                 }
-            }  
-            
+            }
+                              
 
             Console.ReadLine();
 
+        }
+        static private void Mutar(int[] cromosoma,Random rnd)
+        {
+            // Generate two random indices
+            int comienzo = 0;
+            int fin = 0;
+            
+            do
+            {
+                comienzo = rnd.Next(0, cromosoma.Length);
+                fin = rnd.Next(0, cromosoma.Length);                
+            } while (comienzo == fin);
+
+            // Check order
+            if (comienzo > fin)
+            {
+                int temp = comienzo;
+                comienzo = fin;
+                fin = temp;
+            }          
+  
+            // Mutation 
+            int[] copia = new int[cromosoma.Length];
+            Array.Copy(cromosoma, copia, cromosoma.Length);
+            int diferencia = fin - comienzo;            
+            for (int i = 0; i < diferencia; i++)
+            {                
+                cromosoma[comienzo+i] = copia[(fin -1) - i];                
+            }            
         }
         static private void GenerarIndividuos(Individuo[] individuos)
         {
