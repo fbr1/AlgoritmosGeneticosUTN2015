66167f06a215cf96aaaf02ed7bd2c961b2eca5b5
fbr1 <fbr1@users.noreply.github.com>
2015-06-22 21:31:27 -0300

    Busqueda exhaustiva

--- Ejercicios/EjercicioMochila/Program.cs
+++ Ejercicios/EjercicioMochila/Program.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace EjercicioMochila
 {
-    class Program
+    static class Program
     {
         const double VOLUMEN_MAXIMO = 4200;
         static void Main(string[] args)
         {
+            Console.WriteLine("\nAlgortimo Greedy\n");
+
             List<Objeto> objetos = new List<Objeto>(10);
             objetos.Add(new Objeto(150, 20));
             objetos.Add(new Objeto(325, 40));
@@ -29,18 +32,52 @@ namespace EjercicioMochila
             foreach (Objeto o in objetos)
             {
                 suma = suma + o.Volumen;
-                if (suma > 4200) break;
+                if (suma > VOLUMEN_MAXIMO) break;
                 Console.Out.WriteLine(o.Volumen + " " + o.Valor + " " + o.ValorPorVol);
             }
 
-            Console.ReadLine();
-            double max = 0;
-            for (int i = 1; i <= 10; i++)
-            {
-                int[,] subconjunto = new int[i, 2];
+            Console.WriteLine("\nAlgortimo Exaustivo\n");
 
+            double count = Math.Pow(2, objetos.Count); // Cantidad de iteraciones             
+            Int32[] CromosomaMaximo = new Int32[10]; 
+            double valormaximo = 0;
+            for (int i = 0; i < count-1; i++)
+            { // Para cada una de las posibilidades incluyendo repeticion
+                Int32[] cromosoma = ToBinary(i); // Convierte el indice en binario
+                double sumaVolumen = 0;
+                double sumaValor = 0;
+                for (int j = 0; j < cromosoma.Length; j++) // Para cada item del cromosoma
+                {
+                    if (cromosoma[j] == 1) // Si es 1, es un elemento que va a poder llegar a entrar a la mochila
+                    {
+                        sumaVolumen = sumaVolumen + objetos[j].Volumen;
+                        sumaValor = sumaValor + objetos[j].ValorPorVol;                        
+                    }
+                }
+                if (sumaVolumen < VOLUMEN_MAXIMO && sumaValor > valormaximo)
+                {
+                    CromosomaMaximo = cromosoma;
+                }                             
             }
+            for (int i = 0; i < 10; i++)
+            {
+                if (CromosomaMaximo[i] == 1)
+                {
+                    Console.Out.WriteLine(objetos[i].Volumen + " " + objetos[i].Valor + " " + objetos[i].ValorPorVol);
+                }
 
-        }
+
+            } 
+           
+            Console.ReadLine(); 
+       
+        }       
+        public static Int32[] ToBinary(int num)
+        {
+            string s = Convert.ToString(num, 2);
+            char[] bits = s.PadLeft(10, '0').ToCharArray();
+            return Array.ConvertAll(bits, x => int.Parse(x.ToString()));            
+        }        
+        
     }
 }
