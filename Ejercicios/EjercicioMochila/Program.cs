3cf4b7ced41cf71a0214637a070c2c5fd8a581ec
fbr1 <fbr1@users.noreply.github.com>
2015-07-28 18:10:41 -0300

    minor fix

--- Ejercicios/EjercicioMochila/Program.cs
+++ Ejercicios/EjercicioMochila/Program.cs
@@ -13,19 +13,22 @@ namespace EjercicioMochila
         static void Main(string[] args)
         {   
             // Carga de Datos
-            List<Objeto> objetos = new List<Objeto>(10);   
 
-            objetos.Add(new Objeto(150, 20));
-            objetos.Add(new Objeto(325, 40));
-            objetos.Add(new Objeto(600, 50));
-            objetos.Add(new Objeto(805, 36));
-            objetos.Add(new Objeto(430, 25));
-            objetos.Add(new Objeto(1200, 64));
-            objetos.Add(new Objeto(770, 54));
-            objetos.Add(new Objeto(60, 18));
-            objetos.Add(new Objeto(930, 46));
-            objetos.Add(new Objeto(353, 28));   
-                       
+            //objetos.Add(new Objeto(150, 20));
+            //objetos.Add(new Objeto(325, 40));
+            //objetos.Add(new Objeto(600, 50));
+            //objetos.Add(new Objeto(805, 36));
+            //objetos.Add(new Objeto(430, 25));
+            //objetos.Add(new Objeto(1200, 64));
+            //objetos.Add(new Objeto(770, 54));
+            //objetos.Add(new Objeto(60, 18));
+            //objetos.Add(new Objeto(930, 46));
+            //objetos.Add(new Objeto(353, 28));   
+            List<Objeto> objetos = new List<Objeto>();  
+
+            objetos.Add(new Objeto(1800, 72));
+            objetos.Add(new Objeto(600, 36));
+            objetos.Add(new Objeto(1200, 60));  
 
             // Algoritmo greedy
             Console.WriteLine("\nAlgortimo Greedy\n"); 
